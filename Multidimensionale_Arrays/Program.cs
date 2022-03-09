d95dc56bddef5c92c19c8b8ab49e5f8cd39429d6
Nicolai Pascal Mockenhaupt <nicolaipa.m@gso.schule.koeln>
2022-03-09 19:24:08 +0100

    ich brauch ne pause

--- Multidimensionale_Arrays/Program.cs
+++ Multidimensionale_Arrays/Program.cs
@@ -6,22 +6,30 @@ namespace Multidimensionale_Arrays
     {
         static void Main(string[] args)
         {
-            int x, y;
-            int[,] excel = new int[9,5];
+            int x,y,summe = 1;
+            int[,] excel = new int[10,6];
 
             for (int n = 1; n <= 9; n++)
             {
                 Console.Write($"\t{n}");
             }
             Console.WriteLine();
-            for (int i=1; i <= 5; i++)
+
+            for (int i = 1; i <= 5; i++)
             {
-                
-                Console.WriteLine($"{i}\t{i}\t{i*i}\t{i*i*i}\t{i*i*i*i}\t{i*i*i*i*i*i}\t{i*i*i*i*i*i*i}\t{i*i*i*i*i*i*i}\t{i*i*i*i*i*i*i*i}\t{i*i*i*i*i*i*i*i*i}");
+                summe = i;
+                Console.Write($"{summe}");
+                Console.Write($"\t{summe}");
+                for (int n = 1; n < 9; n++)
+                {
 
+                    summe = summe * i;
+                    Console.Write($"\t{summe}");
+                    excel[n, i] = summe;
+
+                }
+                Console.WriteLine();
             }
-
-
         }
     }
 }
