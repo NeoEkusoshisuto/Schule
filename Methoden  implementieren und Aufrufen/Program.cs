c8be2f881c92158c09df56a595fc9eca367b4c4d
Nicolai Pascal Mockenhaupt <nicolaipa.m@gso.schule.koeln>
2022-03-23 12:16:20 +0100

    Methoden

---
+++ Methoden  implementieren und Aufrufen/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Methoden__implementieren_und_Aufrufen
+{
+    class Program
+    {
+        static void Main() 
+        { int anzahl;
+
+           anzahl = ausgabeTextMalX();
+            
+          Console.Write("\nAnzahl der Buchstaben: {0}", anzahl);
+          
+        }
+        
+        public static int ausgabeTextMalX()
+        {
+            
+
+            Console.WriteLine("Welchen Text wollen sie ausgeben lassen?");
+            string text = Console.ReadLine();
+            
+            Console.WriteLine("Wie oft soll der Text ausgegeben werden?");
+            int wiederholungen = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0;i<wiederholungen;i++)
+            {
+                Console.WriteLine(text);
+            }
+
+            int buchstaben = text.Length * wiederholungen;
+
+            return buchstaben;
+        }
+    }
+}
